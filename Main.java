3ecf562b9225e7edcc5a96aa1c0e0ec656c073d3
MaxBlanksby <Max@blanksby.com>
2025-09-15 13:57:32 -0700

    Refactor main method to generate circuit using Gen class before finding best fit

--- Main.java
+++ Main.java
@@ -2,7 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         Solution solution = new Solution();
-        solution.findBestFit(new Circuit());
+        Gen gen = new Gen();
+        Circuit circuit = gen.generateCircuit();
+        solution.findBestFit(circuit);
+
+        
     }
     
 }
